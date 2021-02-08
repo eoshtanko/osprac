7a92aa4ed68c01b83a3a21b1ac237b47973cb754
Shtanko Ekaterina <eoshtanko@edu.hse.ru>
2021-02-08 15:45:13 +0300

    lab 4 done

--- 1.c
+++ 1.c
@@ -1,8 +1,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
-printf("UID = %d\nGID = %d\n", getuid(), getgid());
+int main(int argc, char *argv[], char *envp[])
+{
+for(int i = 0; i < argc; i++){
+printf(argv[i]);
+}
+int i = 0;
+while (envp[i]!=NULL){
+printf(envp[i]);
+i++;
+}
 return 0;
 }
