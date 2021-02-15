6494145534a25bc665d44a195657bd3907d1a6ae
eoshtanko <54939116+eoshtanko@users.noreply.github.com>
2021-02-15 16:55:16 +0300

    5-3.c
    s

---
+++ 5.c
@@ -0,0 +1,82 @@
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+int main() 
+{ 
+int fd[2],fb[2], result; 
+size_t size; 
+char resstring[14]; 
+if (pipe(fd) < 0) { 
+printf("Can\'t first open pipe\n"); 
+exit(-1); 
+} 
+
+if (pipe(fb) < 0) { 
+printf("Can\'t second open pipe\n"); 
+exit(-1); 
+} 
+
+result = fork(); 
+if (result < 0) { 
+printf("Can\'t fork child\n"); 
+exit(-1); 
+} 
+
+if (result > 0) { 
+/* Parent process */ 
+
+if (close(fd[0]) < 0) { 
+printf("parent: Can\'t close reading side of pipe\n"); exit(-1); 
+} 
+
+if (close(fb[1]) < 0) { 
+printf("parent: Can\'t close writing side of pipe\n"); exit(-1); 
+} 
+
+size = write(fd[1], "ABCDEFGHIGKLMN", 14); 
+
+if (size != 14) { 
+printf("Can\'t write all string to pipe\n"); 
+exit(-1); 
+} 
+
+if (close(fd[1]) < 0) { 
+printf("parent: Can\'t close writing side of pipe\n"); exit(-1); 
+} 
+
+size = read(fb[0], resstring, 14); 
+if (size < 0) { 
+printf("Can\'t read string from pipe\n"); 
+exit(-1); 
+} 
+printf("Child exit, result:%s\n", resstring); 
+if (close(fd[0]) < 0) { 
+printf("child: Can\'t close reading side of pipe\n"); exit(-1); 
+} 
+
+} else { 
+/* Child process */ 
+
+if (close(fd[0]) < 0) { 
+printf("parent: Can\'t close reading side of pipe\n"); exit(-1); 
+} 
+
+size = write(fb[1], "ABCDEFGHIGKLMN", 14); 
+
+if (close(fd[1]) < 0) { 
+printf("child: Can\'t close writing side of pipe\n"); exit(-1); 
+} 
+size = read(fd[0], resstring, 14); 
+if (size < 0) { 
+printf("Can\'t read string from pipe\n"); 
+exit(-1); 
+} 
+printf("Child exit, result:%s\n", resstring); 
+if (close(fd[0]) < 0) { 
+printf("child: Can\'t close reading side of pipe\n"); exit(-1); 
+} 
+} 
+
+return 0; 
+}
